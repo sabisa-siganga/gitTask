9c1b2bdd6e20c64115a632b6a57ced2d4f83de4d
Sabisa-Siganga <lumisabisa@gmail.com>
2023-08-23 16:55:19 +0200

    added comments on the code

--- helloWorld.js
+++ helloWorld.js
@@ -1,10 +1,16 @@
+/**
+ * Printing the input from the user
+ * @param {string} input
+ */
 const greetUser = (input) => {
-    const inputFromUser = prompt("Please enter your name", input);
-    
-    if (inputFromUser != null) {
-      document.getElementById("greet").innerHTML =
-      "Hello " + inputFromUser + "!"
-}
-}
-   
+  // prompting the user to enter their name
+  const inputUser = prompt("Please enter your name", input);
+
+  // checking if the input is not empty, thus printing the input from the user
+  if (inputUser != null) {
+    // selecting the span element using id
+    document.getElementById("greet").innerHTML = "Hello " + inputUser + "!";
+  }
+};
+
 greetUser();
